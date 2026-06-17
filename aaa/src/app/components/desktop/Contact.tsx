66a59fd7e5f3dee6497448cfbbfa01dbbfc6e394
Bob <bob@machsyn.com>
2026-06-17 22:25:59 +0000

    revert: remove aaa subdir, actingagency stays standalone

--- aaa/src/app/components/desktop/Contact.tsx
+++
@@ -1,5 +0,0 @@
-// Desktop Contact Section (>1712px)
-// This is the complete contact section copied from SectionContact.tsx
-// Will be used for viewports wider than 1712px
-
-export { default } from "../SectionContact";
